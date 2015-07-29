24e43863a994ddb046c266ef33bc5e57e9e04424
Nirvik Baruah <baruah.nirvik@gmail.com>
2015-07-29 21:29:03 +0800

    Shooting
    Added simple shooting

---
+++ Assets/_Scripts/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	private bool isRight;
+	private Rigidbody2D body;
+	public float speed = 6f;
+
+	void Start(){
+		isRight = PlayerController.isRight;
+		body = GetComponent<Rigidbody2D> ();
+
+		if (isRight){
+			body.velocity = new Vector2(speed, 0);
+		} 
+		else{
+			body.velocity = new Vector2(speed * -1, 0f);
+		}
+	}
+
+	void OnBecameInvisible() {
+		Destroy(gameObject);
+	}
+
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,29 +4,47 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 6f;
+	public static bool isRight = true;
 
 	public float jumpHeight = 7.5f;
 	private int jumpCount = 0;
 
 	private Animator anim;
 	private Rigidbody2D body;
-	private SpriteRenderer sprite;
 
-	public Sprite shootingSprite;
+	public GameObject bullet;
+	private float shootTimer = 5f;
+	private bool shoot = false;
+
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		body = GetComponent<Rigidbody2D> ();
-		sprite = GetComponent<SpriteRenderer> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Movement ();
 
-		if(Input.GetButtonDown("Fire1")) { 
-			Shooting ();
+		if (Input.GetButtonDown("Fire1")) {
+			anim.SetBool ("Shooting", true);
+			shoot = true;
+			shootTimer = 5f;
+
+			Instantiate(bullet, transform.position, Quaternion.identity);
+
+		}
+
+		if (shoot) {
+			if (shootTimer < 0f){
+				anim.SetBool ("Shooting", false);
+				shoot = false;
+			}
+			else{
+				shootTimer -= Time.deltaTime;
+			}
 		}
 	}
 
@@ -35,11 +53,13 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetAxisRaw ("Horizontal") > 0) {
 			transform.Translate(Vector3.right * speed * Time.deltaTime);
 			transform.eulerAngles = new Vector2(0, 0);
+			isRight = true;
 		}
 
 		if (Input.GetAxisRaw ("Horizontal") < 0) {
 			transform.Translate(Vector3.right * speed * Time.deltaTime);
 			transform.eulerAngles = new Vector2(0, 180);
+			isRight = false;
 		}
 		if(Input.GetButtonDown("Jump") && jumpCount == 0) { 
 			body.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
@@ -47,9 +67,6 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	void Shooting(){
-
-	}
 
 	void OnCollisionEnter2D (Collision2D hit) { 
 		if(hit.gameObject.tag == "Floor") { 
