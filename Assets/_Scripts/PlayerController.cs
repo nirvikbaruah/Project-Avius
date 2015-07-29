8cd5fdbb516daa5ac217016f8c5b09fed7b0bc87
Nirvik Baruah <baruah.nirvik@gmail.com>
2015-07-29 10:22:11 +0800

    Animations
    Added animations

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 6f;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement(){
+
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 0);
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") < 0) {
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+	}
+}
