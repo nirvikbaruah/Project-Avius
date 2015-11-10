abbea120a6cc1a5988be5274fc92fc2ecabcd162
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-10 01:46:03 +0000

    Added sprinting (rough)

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,12 @@ public class PlayerMovement : MonoBehaviour {
     public float AirMoveSpeed = 1f;
     public float AirDashForce = 20f;
 
+	public float DoubleTapSpeed = 0.5f;
+
+	float SprintCooler;
+	int SprintTapCount = 0;
+	bool isSprinting = false;
+
     public LayerMask ground;
 
     public float GroundCheckDistance = 1.01f;
@@ -19,16 +25,19 @@ public class PlayerMovement : MonoBehaviour {
 
     Vector3 StartingScale;
 
+
     public void Start()
     {
         RB = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
         StartingScale = transform.localScale;
-
+		SprintCooler = DoubleTapSpeed;
     }
 
     bool grounded = false;
+
+	float LastHorz = 0;
 
     public void Update()
     {
@@ -37,7 +46,36 @@ public class PlayerMovement : MonoBehaviour {
         grounded = hit;
 
         float Horizontal = Input.GetAxis("Horizontal");
-        RB.velocity = new Vector2(Horizontal * (grounded ? WalkSpeed : AirMoveSpeed), RB.velocity.y);
+		float HorzSpeed = Horizontal * (grounded ? WalkSpeed : AirMoveSpeed);
+
+		//Double Tap For sprint
+		if (Mathf.Abs(Horizontal) != 0 && Mathf.Abs(LastHorz) == 0) {
+			Debug.Log ("Tapped");
+			//Tap
+			if (grounded && SprintCooler > 0 && SprintTapCount == 1){
+				Debug.Log ("Started Sprinting");
+				isSprinting = true;
+			} else {
+				SprintCooler = DoubleTapSpeed; 
+				SprintTapCount += 1;
+			}
+		}
+
+		if ( SprintCooler > 0 ) {
+			SprintCooler -= 1 * Time.deltaTime ;
+		} else{
+			SprintTapCount = 0 ;
+		}
+
+		if (Horizontal == 0) {
+			isSprinting = false;
+		}
+
+		if (isSprinting) {
+			HorzSpeed = Horizontal * SprintSpeed;
+		}
+
+		RB.velocity = new Vector2(HorzSpeed, RB.velocity.y);
 
         if (Input.GetButtonDown("Jump") && hit)
         {
@@ -60,6 +98,8 @@ public class PlayerMovement : MonoBehaviour {
             anim.SetBool("Jumped", !grounded);
             anim.SetBool("Landed", grounded);
         }
+
+		LastHorz = Horizontal;
     }
 
     void OnDrawGizmos()
